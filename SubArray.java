ba23ac415f11754b28c6500fb9bed3d0d2da9777
sampleAdi <128214216+sampleAdi@users.noreply.github.com>
2023-05-29 11:11:30 +0530

    Create SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SubArray {
+    static int ts = 0;
+
+    public static void subarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of subarrays: " + ts);
+    }
+
+    public static void SB(int arr[]) {
+        int n = arr.length;
+        int temp = (n * (n + 1)) / 2;
+        System.out.println("total no. of subarrays: " + temp);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10 };
+        subarr(arr);
+        SB(arr);
+    }
+}
