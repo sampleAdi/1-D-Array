26fea0d4c02dd189dff18b82c68f4872374098fa
sampleAdi <128214216+sampleAdi@users.noreply.github.com>
2024-06-13 11:49:15 +0530

    Update MaxSubArraySum.java

--- MaxSubArraySum.java
+++ MaxSubArraySum.java
@@ -2,13 +2,11 @@ import java.util.*;
 
 public class MaxSubArraySum {
     public static void MaxSub(int arr[]) {
-        int sum = 0, currsum = Integer.MIN_VALUE, start, end, i;
-        for (i = 0; i < arr.length; i++) {
+        int sum = 0, currsum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
             sum = 0;
-            start = i;
-            end = arr.length - 1;
-            for (; start <= end; start++) {
-                sum = sum + arr[start];
+            for (int j=i;j<arr.length;j++) {
+                sum = sum + arr[j];
             }
             if (sum > currsum) {
                 currsum = sum;
@@ -18,7 +16,7 @@ public class MaxSubArraySum {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 6, 8, 10 };
+        int arr[] = { -1,2,1,2,3 };
         MaxSub(arr);
     }
 }
